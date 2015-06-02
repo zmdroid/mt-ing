b9dff97c7ceea942c86d93ae42281fb3e1762b3d
Dario Daić <dariodaic5.1@gmail.com>
2015-06-02 04:20:55 +0200

    Adding skewer class, active_item functionality and setting general layout.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    @home_active = true
   end
 
   def about
+    @about_active = true
   end
 
   def products
+    @products_active = true
   end
 
   def contact
+    @contact_active = true
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,5 @@
 module StaticPagesHelper
+  def activate_link(page, text, path)
+    page ? link_to(text, path, class: "active_item") : link_to(text, path)
+  end
 end

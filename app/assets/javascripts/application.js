5e818c916897ea8da565b7ea14f0c0d53048eb5b
Dario Daić <dariodaic5.1@gmail.com>
2015-06-07 23:15:21 +0200

    Adding sidebar functionality.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,13 @@
 //= require bootstrap
 //= require_tree .
 
-$( document  ).ready(function() {
-
+$(document).ready(function() {
+  $(".sidebar_item").click(function() {
+    var itemIsClosed = $(this).hasClass("closed");
+    if (itemIsClosed) {
+      $(this).removeClass("closed").addClass("opened");
+    } else {
+      $(this).removeClass("opened").addClass("closed");
+    }
+  });
 });

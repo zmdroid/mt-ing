ff44e5ca7f4582c5f46f51a4f7d1a8180dfbc1d6
dariodaic <dariodaic5.1@gmail.com>
2015-06-14 18:08:43 +0200

    Fixing homepage sidebar class for xs and sm.

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,9 +1,19 @@
 class EnquiriesController < ApplicationController
   def create
-    if @enquiry.save
-      EnquiryMailer.notify(@enquiry).deliver_now
+    enquiry = Enquiry.new(enquiry_params)
+
+    if enquiry.save
+      EnquiryMailer.notify(enquiry).deliver_now
+      redirect_to # ???
     else
       render 'contact'
     end
   end
+
+  private
+
+    def enquiry_params
+      params.require(:enquiry).permit(:name, :email, :service_description,
+                                      :phone, :entity, :budget, :deadline)
+    end
 end

afa831a449d6403813d7e4c879b73a33f80e1d20
dariodaic <dariodaic5.1@gmail.com>
2015-06-14 22:17:39 +0200

    Switching sidebar functionality.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,14 +2,3 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
-
-$(document).ready(function() {
-  $(".sidebar_item").click(function() {
-    var itemIsClosed = $(this).hasClass("closed");
-    if (itemIsClosed) {
-      $(this).removeClass("closed").addClass("opened");
-    } else {
-      $(this).removeClass("opened").addClass("closed");
-    }
-  });
-});
--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $(".sidebar_item").click(function() {
+    var clickedItem = $(this);
+    if (clickedItem.hasClass("opened")) {
+      clickedItem.removeClass("opened").addClass("closed");
+      return;
+     }
+     var sidebarItems = $(".sidebar_item");
+     for (var i = 0; i < sidebarItems.length; i++) {
+       var item = $(sidebarItems[i]);
+       if (item.hasClass("opened")) {
+         item.removeClass("opened").addClass("closed");
+         break;
+       }
+     }
+     if (clickedItem.hasClass("closed")) {
+       clickedItem.removeClass("closed").addClass("opened");
+     }
+  });
+});

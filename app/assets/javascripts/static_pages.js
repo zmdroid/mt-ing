1701632cba590773d1cc3bcf577186a1505d04b1
dariodaic <dariodaic5.1@gmail.com>
2015-07-03 16:00:11 +0200

    adding spin to sidebar arrows

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var clickedItem = $(this);
     if (clickedItem.hasClass("opened")) {
       clickedItem.removeClass("opened").addClass("closed");
+      clickedItem.find("img").removeClass("rotate-right").addClass("rotate-left");
       return;
      }
      var sidebarItems = $(".sidebar_item");
@@ -10,11 +11,18 @@ $(document).ready(function() {
        var item = $(sidebarItems[i]);
        if (item.hasClass("opened")) {
          item.removeClass("opened").addClass("closed");
+         item.find("img").removeClass("rotate-right").addClass("rotate-left");
          break;
        }
      }
      if (clickedItem.hasClass("closed")) {
        clickedItem.removeClass("closed").addClass("opened");
+       var img = clickedItem.find("img");
+       if (img.hasClass("rotate-left")) {
+         img.removeClass("rotate-left").addClass("rotate-right");
+       } else {
+         img.addClass("rotate-right");
+       }
      }
   });
 });

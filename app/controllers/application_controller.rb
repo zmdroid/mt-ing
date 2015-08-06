ba93840da8c72ad2481b9a5140e1ed5699951e3b
dariodaic <dariodaic5.1@gmail.com>
2015-08-06 03:10:13 +0200

    finishing 'about' > 'employees' static page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_admin
 
   def current_admin
     @current_admin ||= Admin.find_by_id(session[:admin_id]) if session[:admin_id]
